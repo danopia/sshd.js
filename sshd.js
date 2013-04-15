4ffe4acceb715a88dfdeb64c41017ee4e27c543a
Daniel Lamando <me@danopia.net>
2013-04-15 02:24:06 -0400

    Handle putty DF, add auth, nyan shell

--- sshd.js
+++ sshd.js
@@ -15,7 +15,7 @@ function signBuffer(buffer) {
 }
 
 require('net').createServer(function (conn) {
-  var macLen = 0, seqS = 0, seqC = 0, kex, dh, hashIn = [], keyson = false, session, cookie, deciph, cipher, macS, macC, user, proc;
+  var macLen = 0, seqS = 0, seqC = 0, kex, dh, hashIn = [], keyson = false, session, cookie, deciph, cipher, macS, macC, user, proc, keys = [], stage;
   
   var sendPay = function (payload) {
     var padLen = (16-((5 + payload.length)%16))+16;
@@ -70,6 +70,19 @@ require('net').createServer(function (conn) {
           cprAlgs:    [packet.readList(), packet.readList()],
           langs:      [packet.readList(), packet.readList()],
           firstKexFollows: packet.readBool()};
+        console.log(kex);
+        break;
+      
+      case 30: // older 34
+        var dhflags = { n:   packet.readUInt32() };
+        hashIn.push({uint32: dhflags.n});
+        dh = require('crypto').getDiffieHellman('modp2');
+        
+        // SSH_MSG_KEX_DH_GEX_GROUP
+        hashIn.push({mpint: dh.getPrime()});
+        hashIn.push({mpint: new Buffer([2])});
+        sendPay(composePacket([{byte: 31}, {mpint: dh.getPrime()}, {mpint: new Buffer([2])}]));
+        dh.generateKeys();
         break;
       
       case 34: // SSH_MSG_KEX_DH_GEX_REQUEST
@@ -138,7 +151,55 @@ require('net').createServer(function (conn) {
         var service = packet.readString();
         var method = packet.readString(); // plus more
         console.log(user, service, method);
-        sendPayload([{byte: 52}]); // SSH_MSG_USERAUTH_SUCCESS
+        if (method == 'none') {
+          if (true) { // anonymous server?
+            sendPayload([{byte: 52}]); // SSH_MSG_USERAUTH_SUCCESS
+          } else {
+            sendPayload([{byte: 51}, ['publickey', 'keyboard-interactive'], false]); // SSH_MSG_USERAUTH_FAILURE
+          };
+        } else if (method == 'keyboard-interactive') {
+          var lang = packet.readString();
+          var submethods = packet.readString();
+          console.log({lang:lang,submethods:submethods});
+          if (keys.length) {
+            sendPayload([{byte: 60}, 'Log in to Gitbus', "Your "+keys.length+" public keys were not recognized by gitbus. If you'd like to add one, please log in.", 'en-US', {uint32:2}, 'Username: ', true, 'Password: ', false]); // SSH_MSG_USERAUTH_INFO_REQUEST
+          } else {
+            sendPayload([{byte: 51}, ['publickey'], false]); // SSH_MSG_USERAUTH_FAILURE
+          }
+        } else if (method == 'password') {
+          sendPayload([{byte: 53}, 'Welcome to Gitbus!\r\n', 'en-US']);
+          sendPayload([{byte: 53}, "I don't recognize your SSH public key.\r\n", 'en-US']);
+          sendPayload([{byte: 53}, "If you'd like to pair it to " + user + ", enter your gitbus password now.\r\n", 'en-US']);
+        } else if (method == 'publickey') {
+          var signed = packet.readBool();
+          var key = {
+            alg: packet.readString(),
+            blob: packet.readString()};
+          console.log(key);
+          keys.push(key);
+          sendPayload([{byte: 51}, ['publickey', 'keyboard-interactive'], false]); // SSH_MSG_USERAUTH_FAILURE
+        } else {
+          sendPayload([{byte: 51}, ['publickey', 'keyboard-interactive', 'password'], false]); // SSH_MSG_USERAUTH_FAILURE
+        };
+        break;
+      
+      case 61: // SSH_MSG_USERAUTH_INFO_RESPONSE
+        var count = packet.readUInt32();
+        if (stage) {
+//          var keynum = packet.readString();
+//          var comment = packet.readString();
+          sendPayload([{byte: 53}, 'Key successfully added.\r\n', 'en-US']);
+          sendPayload([{byte: 52}]); // SSH_MSG_USERAUTH_INFO_REQUEST
+        } else {
+          var username = packet.readString();
+          var password = packet.readString();
+          if (keys.length > 1) {
+            sendPayload([{byte: 60}, 'Select public key to add', keys.map(function(key,i){return ''+i+': '+key.alg+' '+(new Buffer(key.blob).toString('base64')).slice(0,60)+'...'}).join('\r\n'), 'en-US', {uint32:2}, 'Key number: ', true, 'Key label/comment: ', true]); // SSH_MSG_USERAUTH_INFO_REQUEST
+          } else {
+            sendPayload([{byte: 60}, 'Add key to Gitbus', keys[0].alg+' '+(new Buffer(keys[0].blob).toString('base64')).slice(0,60)+'... will be added to your Gitbus.', 'en-US', {uint32:1}, 'Key label/comment: ', true]); // SSH_MSG_USERAUTH_INFO_REQUEST
+          };
+          stage = true;
+        };
         break;
       
       case 80: // SSH_MSG_GLOBAL_REQUEST
@@ -210,14 +271,17 @@ require('net').createServer(function (conn) {
           };
           
           /*/
-          var cp = require('child_process').spawn('cowsay');
-          cp.stdin.write(bin);
-          cp.stdin.end();
-          cp.stdout.on('data', function (d) {
+          proc = require('child_process').spawn('cowsay');
+          proc.stdin.write(bin);
+          proc.stdin.end();
+          proc.stdout.on('data', function (d) {
             console.log(d);
             sendPayload([{byte: 94}, {uint32: recip}, d]);
           }).setEncoding('utf8');
-          cp.on('exit', function (code, signal) {
+          proc.on('error', function (err) {
+            console.log('Child process hit an error.', err);
+          });
+          proc.on('exit', function (code, signal) {
             if (code !== null) {
               sendPayload([{byte: 98}, {uint32: recip}, 'exit-status', false, {uint32: code}]); // SSH_MSG_CHANNEL_REQUEST
             };
@@ -239,6 +303,22 @@ require('net').createServer(function (conn) {
         } else if (type == 'shell') {
           console.log('Client warms up their shell');
           sendPayload([{byte: 99}, {uint32: recip}]); // SSH_MSG_CHANNEL_SUCCESS
+          
+          sendPayload([{byte: 94}, {uint32: recip}, '\e[48;5;17m']);
+          proc = require('child_process').spawn('nyancat');
+          proc.stdout.on('data', function (d) {
+            sendPayload([{byte: 94}, {uint32: recip}, d.replace(/\n/g, '\r\n')]);
+          }).setEncoding('utf8');
+          proc.on('exit', function (code, signal) {
+            if (code !== null) {
+              sendPayload([{byte: 98}, {uint32: recip}, 'exit-status', false, {uint32: code}]); // SSH_MSG_CHANNEL_REQUEST
+            };
+            
+            sendPayload([{byte: 97}, {uint32: recip}]);
+          });
+          proc.on('error', function (err) {
+            console.log('Child process hit an error.', err);
+          });
         } else {
           console.log('Requested', type, 'for', recip, '... but idk');
           if (wantReply)
@@ -246,15 +326,23 @@ require('net').createServer(function (conn) {
         };
         break;
       
+      case 93: // SSH_MSG_CHANNEL_WINDOW_ADJUST
+        break;
+      
       case 94: // SSH_MSG_CHANNEL_DATA
         var chan = packet.readUInt32();
         var data = packet.readString();
         console.log(chan, data);
         if (proc) {
-          while (data.length) {
-            proc.stdin.write(data.slice(0, 512));
-            data = data.slice(512);
-          }
+          if (data == '\u0003') {
+            proc.kill('SIGINT');
+            proc = null;
+          } else {
+            while (data.length) {
+              proc.stdin.write(data.slice(0, 512));
+              data = data.slice(512);
+            };
+          };
         } else {
           if (data == '\u0004') {
             sendPayload([{byte: 94}, {uint32: chan}, 'Hit q to exit\r\n']);
@@ -276,6 +364,10 @@ require('net').createServer(function (conn) {
   
   conn.on('error', function (err) {
     console.log('Connection closed due to error.', err);
+  });
+  conn.on('close', function (err) {
+    console.log('Connection closed.');
+    if (proc) proc.kill();
   });
   
   console.log('New connection');
